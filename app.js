80a2781bc2f46913ac3750d01c71b5d65e84911f
narasihistorian <narasihistorian@gmail.com>
2024-05-10 03:21:59 +0700

    fix revision 10 may 2024

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 const lenis = new Lenis();
 
-lenis.on('scroll', (e) => {
+lenis.on("scroll", (e) => {
   console.log(e);
 });
 
@@ -17,37 +17,53 @@ requestAnimationFrame(raf);
 
 // navbar burger toggle mobile version -------------------------------------------
 
-const menuBurger = document.querySelector('.burger');
+const menuBurger = document.querySelector(".burger");
+const nav = document.querySelector(".menu-navbar");
+const links = nav.querySelectorAll(".menu-navbar li a");
 
 let menuStatus = false;
 
-const menuTL = gsap.timeline({
-  default: { duration: 0.5, ease: 'power4.inOut' },
+// menu tlopen --------------------
+
+const menuTLOpen = gsap.timeline({
+  default: { duration: 0.5, ease: "power4.inOut" },
 });
 
-menuTL.to('.menu-navbar', {
+menuTLOpen.to(".menu-navbar", {
   scaleY: 1,
   stagger: 0.5,
 });
 
-menuTL.to('.line1', { rotateZ: '35deg' }, '<');
-menuTL.to('.line3', { rotateZ: '-35deg', y: '-10px' }, '<');
-menuTL.to('.line2', { opacity: 0, onComplete: fadeIn }, '<');
+menuTLOpen.to(".line1", { rotateZ: "35deg" }, "<");
+menuTLOpen.to(".line3", { rotateZ: "-35deg", y: "-10px" }, "<");
+menuTLOpen.to(".line2", { opacity: 0, onComplete: fadeIn }, "<");
 
 function fadeIn() {
-  menuTL.to('.menu-navbar li', { opacity: 1, duration: 0.5 });
+  menuTLOpen.to(".menu-navbar li", { opacity: 1, duration: 0.5 });
 }
 
-menuTL.paused(true);
+menuTLOpen.paused(true);
 
-menuBurger.addEventListener('click', () => {
+menuBurger.addEventListener("click", () => {
   if (!menuStatus) {
-    menuTL.play();
+    menuTLOpen.play();
     menuStatus = true;
   } else {
-    menuTL.reverse();
+    menuTLOpen.reverse();
     menuStatus = false;
   }
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (!menuStatus) {
+      menuTLOpen.play();
+      menuStatus = true;
+    } else {
+      menuTLOpen.reverse();
+      menuStatus = false;
+    }
+  });
 });
 
 // matchmedia responsive --------------------------------------------------
@@ -56,8 +72,8 @@ const matchMediaResponsive = gsap.matchMedia();
 
 matchMediaResponsive.add(
   {
-    isDesktop: '(min-width: 1025px)',
-    isMobile: '(max-width: 1024px)',
+    isDesktop: "(min-width: 1025px)",
+    isMobile: "(max-width: 1024px)",
   },
   (context) => {
     console.log(context.conditions);
@@ -66,7 +82,7 @@ matchMediaResponsive.add(
     // navbar closed ---------------------------------------------------------------
 
     const showAnim = gsap
-      .from('nav', {
+      .from("nav", {
         yPercent: -100,
         paused: true,
         duration: 0.2,
@@ -74,7 +90,7 @@ matchMediaResponsive.add(
       .progress(1);
 
     ScrollTrigger.create({
-      start: 'top top',
+      start: "top top",
       end: 99999,
       onUpdate: (self) => {
         self.direction === -1 ? showAnim.play() : showAnim.reverse();
@@ -85,18 +101,16 @@ matchMediaResponsive.add(
 
     // scene 1 ----------------------------------------------------
 
-    const textPembuka = new SplitType('.title-animation-1 h1');
-    const textPembuka2 = new SplitType('.title-animation-2 h1');
+    const textPembuka = new SplitType(".title-animation-1 h1");
 
     const tl = gsap.timeline({
-      defaults: { duration: 0.75, ease: 'power4.inOut' },
+      defaults: { duration: 0.75, ease: "power4.inOut" },
     });
 
-    gsap.set('.title-animation-1 h1', { autoAlpha: 1 });
-    gsap.set('.title-animation-2 h1', { autoAlpha: 1 });
+    gsap.set(".title-animation-1 h1", { autoAlpha: 1 });
 
-    tl.to(['#motto', '#service', '#booking'], {
-      display: isMobile ? 'block' : 'none',
+    tl.to(["#motto", "#service", "#booking"], {
+      display: isMobile ? "block" : "none",
     });
 
     if (isDesktop) {
@@ -107,9 +121,9 @@ matchMediaResponsive.add(
           opacity: 0,
           skewX: 30,
           stagger: 0.03,
-          duration: 1.5,
+          duration: 0.5,
         },
-        '<'
+        "<"
       );
 
       tl.to(textPembuka.chars, {
@@ -120,35 +134,20 @@ matchMediaResponsive.add(
         duration: 0.5,
       });
 
-      tl.to('.title-animation-2 h1', { display: 'block' });
-
-      tl.from(textPembuka2.chars, {
-        y: 40,
-        opacity: 0,
-        skewX: 30,
-        stagger: 0.03,
-        duration: 1.5,
-      }).to(textPembuka2.chars, {
-        y: -40,
-        opacity: 0,
-        skewX: -10,
-        stagger: 0.03,
-        duration: 0.5,
-      });
-
-      tl.to('.title-animation-3 h1', { display: 'block' });
-
-      tl.to('.title-animation-3 h1', {
+      tl.to(".title-animation-2 h1", {
         opacity: 1,
         duration: 1,
-      }).to('.title-animation-3 h1', {
+      }).to(".title-animation-2 h1", {
         opacity: 0,
         duration: 1,
       });
     } else {
       gsap.set(
-        ['.title-animation-1', '.title-animation-2', '.title-animation-3'],
-        { display: 'none' }
+        [".title-animation-1", ".title-animation-2"],
+        {
+          display: "none",
+        },
+        "<"
       );
     }
 
@@ -156,113 +155,153 @@ matchMediaResponsive.add(
 
     if (isDesktop) {
       tl.to(
-        '.background-video',
+        ".background-video",
         {
           scaleX: 1,
           delay: 1,
           duration: 1,
         },
-        '-=.2'
+        "-=1.5"
       )
         .to(
-          '.background-video',
+          ".background-video",
           {
-            height: '100vh',
+            height: "100vh",
             bottom: 0,
             top: 0,
             delay: 1,
             duration: 1,
           },
-          '-=.5'
+          "-=.5"
         )
-        .to('.background-video', { border: 'none' }, '-=.5');
+        .to(".background-video", { border: "none" }, "-=.5");
 
       tl.to(
-        '.main-title-mask h2',
+        ".main-title-mask h1",
         {
           x: isMobile ? 1 : 0,
           opacity: 1,
           duration: 1.5,
         },
-        '<'
+        "<"
       ).to(
-        '.sub-title-mask h3',
+        ".sub-title-mask h3",
         { y: isMobile ? 1 : 0, opacity: 1, duration: 1.5 },
-        '<'
+        "<"
       );
 
       tl.to(
-        '.main-title-mask h2',
-        { scale: isMobile ? 1 : 0.9, transformOrigin: 'center' },
-        '-=.5'
+        ".main-title-mask h1",
+        { scale: isMobile ? 1 : 0.9, transformOrigin: "center" },
+        "-=.5"
       );
       tl.to(
-        '.sub-title-mask h3',
-        { scale: isMobile ? 1 : 0.9, transformOrigin: 'center' },
-        '<'
+        ".sub-title-mask h3",
+        { scale: isMobile ? 1 : 0.9, transformOrigin: "center" },
+        "<"
       );
-      tl.to('.main-home button', { scaleY: 1, duration: 0.5 }, '-=.9');
+      tl.to(".main-home button", { scaleY: 1, duration: 0.5 }, "-=.9");
       tl.from(
-        '.logo-navbar',
+        ".logo-navbar",
         {
-          x: isMobile ? 0 : '-100%',
+          x: isMobile ? 0 : "-100%",
           opacity: isMobile ? 1 : 0.5,
           duration: 0.5,
         },
-        '-=.7'
+        "-=.7"
       );
-      tl.to('.menu-navbar', { y: 0 }, '-=.7');
-      tl.to('.fade-container', { display: 'none' }, '-=.7');
+      tl.to(".menu-navbar", { y: 0 }, "-=.7");
+      tl.to(".fade-container", { display: "none" }, "-=.7");
 
-      tl.to(['#motto', '#service', '#booking'], { display: 'block' });
+      tl.to(["#motto", "#service", "#booking"], { display: "block" });
     } else {
-      gsap.set('.background-video', {
-        height: '100vh',
+      gsap.set(".background-video", {
+        height: "100vh",
         top: 0,
         bottom: 0,
         scaleX: 1,
       });
-      gsap.set('.main-home h2', {
+      gsap.set(".main-home h1", {
         x: 0,
-        transformOrigin: 'left',
+        transformOrigin: "left",
         opacity: 1,
       });
-      gsap.set('.main-home h3', {
+      gsap.set(".main-home h3", {
         y: 0,
         opacity: 1,
       });
-      gsap.set('.main-home button ', {
+      gsap.set(".main-home button ", {
         scaleY: 1,
       });
     }
 
+    // close home with opacity -----------------------------------------
+
+    const tlhomeOpacity = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#home",
+        start: "80% center",
+        end: "bottom top",
+        scrub: 4,
+
+        // the 4 states of toggle actions
+
+        // onEnter - this actions happens when you scrool into specific sections or element for the first time. scroolling down
+        // onLeave - this actions happens when you scrool out of a specific sections or element for the frist time. scrooling down
+        // onEnterBack - this actions happens when you scrool back into a specific sections or element for the first time.  scrooling up
+        // onLeaveBack - this actions happens when you scrool back out of a specific sections or elemetn for the first time . scrolling up
+        // value: "play", "resume", "reset", "restart", complete, "reverse", and "none"
+
+        // toggleActions: "play complete play reverse",
+
+        // markers: true,
+      },
+      default: { ease: "power3.out", duration: 1 },
+    });
+
+    tlhomeOpacity.to("#home", { opacity: isMobile ? 1 : 0, duration: 0.8 });
+
     // Description sections -----------------------------------------
 
-    const mainTextMottoSections = new SplitType('.highlight');
-    const mainText2MottoSections = new SplitType('.highlight1');
-    const paragraphMottoSections = new SplitType('.motto-container p');
+    const mainTextMottoSections = new SplitType(".highlight");
+    const mainText2MottoSections = new SplitType(".highlight1");
 
     const tlMottoSections = gsap.timeline({
       scrollTrigger: {
-        trigger: '#motto',
-        start: '0%',
-        end: '10%',
+        trigger: "#motto",
+        start: "-35% center",
+
+        // the 4 states of toggle actions
+
+        // onEnter - this actions happens when you scrool into specific sections or element for the first time. scroolling down
+        // onLeave - this actions happens when you scrool out of a specific sections or element for the frist time. scrooling down
+        // onEnterBack - this actions happens when you scrool back into a specific sections or element for the first time.  scrooling up
+        // onLeaveBack - this actions happens when you scrool back out of a specific sections or elemetn for the first time . scrolling up
+        // value: "play", "resume", "reset", "restart", complete, "reverse", and "none"
+
+        toggleActions: "play complete restart reverse",
+
         // markers: true,
       },
-      default: { ease: 'power3.out', duration: 1 },
+      default: { ease: "power3.out", duration: 2 },
     });
 
-    gsap.set('.highlight', { autoAlpha: 1 });
-    gsap.set('.highlight1', { autoAlpha: 1 });
-    gsap.set('.motto-container p', { autoAlpha: 1 });
+    gsap.set(".highlight", { autoAlpha: 1 });
+    gsap.set(".highlight1", { autoAlpha: 1 });
 
-    tlMottoSections.from(mainTextMottoSections.chars, {
-      y: isMobile ? 0 : 40,
-      opacity: isMobile ? 1 : 0,
-      skewX: isMobile ? 0 : 30,
-      stagger: 0.03,
-      duration: 0.8,
-    });
+    tlMottoSections.from(".background-motto", { opacity: 0, duration: 1 });
+
+    tlMottoSections.from(
+      mainTextMottoSections.chars,
+      {
+        y: isMobile ? 0 : 40,
+        opacity: isMobile ? 1 : 0,
+        skewX: isMobile ? 0 : 30,
+        stagger: 0.03,
+        duration: 0.8,
+      },
+      "-=.5"
+    );
 
     tlMottoSections.from(
       mainText2MottoSections.chars,
@@ -273,27 +312,25 @@ matchMediaResponsive.add(
         stagger: 0.03,
         duration: 0.9,
       },
-      '-=.8'
+      "-=.8"
     );
 
     tlMottoSections.from(
-      paragraphMottoSections.lines,
+      ".motto-container p",
       {
         y: isMobile ? 0 : 40,
         opacity: isMobile ? 1 : 0,
-        skewX: isMobile ? 0 : 30,
-        stagger: 0.03,
-        duration: 0.4,
+        duration: 0.7,
       },
-      '-=.8'
+      "-=.8"
     );
 
     // service sections -----------------------------------------
 
-    const imgServiceSections = gsap.utils.toArray('#service img');
-    const mainTitleServiceSections = gsap.utils.toArray('#service h1');
-    const paragraphServiceSections = gsap.utils.toArray('#service p');
-    const buttonServiceSections = gsap.utils.toArray('#service button');
+    const imgServiceSections = gsap.utils.toArray("#service img");
+    const mainTitleServiceSections = gsap.utils.toArray("#service h1");
+    const paragraphServiceSections = gsap.utils.toArray("#service p");
+    const buttonServiceSections = gsap.utils.toArray("#service button");
 
     // main title
 
@@ -301,8 +338,7 @@ matchMediaResponsive.add(
       const tlService = gsap.timeline({
         scrollTrigger: {
           trigger: img,
-          start: '-50%',
-          end: 'bottom center',
+          start: "top bottom",
 
           // the 4 states of toggle actions
 
@@ -312,17 +348,17 @@ matchMediaResponsive.add(
           // onLeaveBack - this actions happens when you scrool back out of a specific sections or elemetn for the first time . scrolling up
           // value: "play", "resume", "reset", "restart", complete, "reverse", and "none"
 
-          // toggleActions: 'play play none reverse',
+          toggleActions: "play complete restart reverse",
           // markers: true,
         },
       });
 
       tlService.from(img, {
         duration: isMobile ? 0 : 1,
-        x: isMobile ? 0 : '-50',
+        x: isMobile ? 0 : "-50",
         opacity: isMobile ? 1 : 0,
         stagger: 0.005,
-        ease: 'power2.out',
+        ease: "power2.out",
       });
     });
 
@@ -334,8 +370,7 @@ matchMediaResponsive.add(
       const tlService = gsap.timeline({
         scrollTrigger: {
           trigger: text,
-          start: '-300%',
-          end: 'bottom center',
+          start: "top bottom",
 
           // the 4 states of toggle actions
 
@@ -345,7 +380,7 @@ matchMediaResponsive.add(
           // onLeaveBack - this actions happens when you scrool back out of a specific sections or elemetn for the first time . scrolling up
           // value: "play", "resume", "reset", "restart", complete, "reverse", and "none"
 
-          // toggleActions: 'play play none reverse',
+          toggleActions: "play complete restart reverse",
           // markers: true,
         },
       });
@@ -355,20 +390,17 @@ matchMediaResponsive.add(
         y: isMobile ? 0 : 50,
         opacity: isMobile ? 1 : 0,
         stagger: 0.005,
-        ease: 'power2.out',
+        ease: "power2.out",
       });
     });
 
     // paragraph title
 
-    paragraphServiceSections.forEach((text) => {
-      const splitType = new SplitType(text);
-
+    paragraphServiceSections.forEach((paragraph) => {
       const tlService = gsap.timeline({
         scrollTrigger: {
-          trigger: text,
-          start: '-220%',
-          end: 'bottom center',
+          trigger: paragraph,
+          start: "top bottom",
 
           // the 4 states of toggle actions
 
@@ -378,17 +410,16 @@ matchMediaResponsive.add(
           // onLeaveBack - this actions happens when you scrool back out of a specific sections or elemetn for the first time . scrolling up
           // value: "play", "resume", "reset", "restart", complete, "reverse", and "none"
 
-          // toggleActions: 'play play none reverse',
+          toggleActions: "play complete restart reverse",
           // markers: true,
         },
       });
 
-      tlService.from(splitType.chars, {
+      tlService.from(paragraph, {
         duration: 1,
-        y: isMobile ? 0 : 50,
+        y: isMobile ? "0%" : "50%",
         opacity: isMobile ? 1 : 0,
-        stagger: 0.005,
-        ease: 'power2.out',
+        ease: "power2.out",
       });
     });
 
@@ -398,8 +429,7 @@ matchMediaResponsive.add(
       const tlService = gsap.timeline({
         scrollTrigger: {
           trigger: button,
-          start: '-1000%',
-          end: 'bottom center',
+          start: "top bottom",
 
           // the 4 states of toggle actions
 
@@ -409,7 +439,7 @@ matchMediaResponsive.add(
           // onLeaveBack - this actions happens when you scrool back out of a specific sections or elemetn for the first time . scrolling up
           // value: "play", "resume", "reset", "restart", complete, "reverse", and "none"
 
-          // toggleActions: 'play play none reverse',
+          toggleActions: "play complete restart reverse",
           // markers: true,
         },
       });
@@ -418,8 +448,61 @@ matchMediaResponsive.add(
         duration: 0.5,
         scaleX: isMobile ? 1 : 0,
         opacity: isMobile ? 1 : 0.5,
-        ease: 'power2.out',
+        ease: "power2.out",
       });
     });
+
+    // closing sections -----------------------------------------
+
+    const textClosingTitle = new SplitType(".main-booking h1");
+
+    gsap.set(".main-booking h1", { autoAlpha: 1 });
+
+    const tlClosing = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#booking",
+        start: "top center",
+
+        // the 4 states of toggle actions
+
+        // onEnter - this actions happens when you scrool into specific sections or element for the first time. scroolling down
+        // onLeave - this actions happens when you scrool out of a specific sections or element for the frist time. scrooling down
+        // onEnterBack - this actions happens when you scrool back into a specific sections or element for the first time.  scrooling up
+        // onLeaveBack - this actions happens when you scrool back out of a specific sections or elemetn for the first time . scrolling up
+        // value: "play", "resume", "reset", "restart", complete, "reverse", and "none"
+
+        toggleActions: "play complete restart reverse",
+        // markers: true,
+      },
+    });
+
+    tlClosing.from(textClosingTitle.chars, {
+      y: isMobile ? 0 : 40,
+      opacity: isMobile ? 1 : 0,
+      skewX: isMobile ? 0 : 30,
+      stagger: 0.03,
+      duration: 1.5,
+    });
+
+    tlClosing.from(
+      ".main-booking p",
+      {
+        y: isMobile ? 0 : 40,
+        opacity: isMobile ? 1 : 0,
+        duration: 1.5,
+      },
+      "<"
+    );
+
+    tlClosing.from(
+      ".main-booking button",
+      {
+        scale: isMobile ? 1 : 0,
+        ease: "bounce.out",
+        duration: 1,
+        delay: 0.5,
+      },
+      "<"
+    );
   }
 );
